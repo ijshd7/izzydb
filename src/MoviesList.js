010272b497cbc3d2210dbeb7194a1069e15e5483
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2020-02-19 18:44:38 -0600

    did it 4 the lolz

---
+++ src/MoviesList.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import Movie from './Movie';
+
+
+class MoviesList extends PureComponent {
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=ccd177fb9015c5f032160d86c9f22d86&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      })
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+        <MovieGrid>
+            {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+    display: grid;
+    padding: 1rem;
+    grid-template-columns: repeat(5, 1fr);
+    grid-row-gap: 1rem;
+`;
